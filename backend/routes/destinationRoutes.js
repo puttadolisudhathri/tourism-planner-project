08110ea729f434e9d7777f5160e618d040480277
puttadolisudhathri <puttadolisudhathri@gmail.com>
2026-09-21 20:08:31 +0530

    Update destinationRoutes.js

--- backend/routes/destinationRoutes.js
+++ backend/routes/destinationRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
-const Destination = require("../models/destination");
+const Destination = require("../models/Destination");
 
 const router = express.Router();
 
